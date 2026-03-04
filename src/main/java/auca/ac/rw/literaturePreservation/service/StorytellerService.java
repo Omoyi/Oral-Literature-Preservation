eca0188a7bd2373e875e250cdf8b11f9fb2f87fc
Omoyi <urakhel7@gmail.com>
2026-03-04 12:04:52 +0200

    Save storyteller location, get all by Id and location

--- src/main/java/auca/ac/rw/literaturePreservation/service/StorytellerService.java
+++ src/main/java/auca/ac/rw/literaturePreservation/service/StorytellerService.java
@@ -1,5 +1,44 @@
 package auca.ac.rw.literaturePreservation.service;
 
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import auca.ac.rw.literaturePreservation.domain.*;
+import auca.ac.rw.literaturePreservation.repository.*;
+import auca.ac.rw.literaturePreservation.service.*;
+
+@Service
 public class StorytellerService {
 
+    @Autowired
+    private StorytellerRepository storytellerRepository;
+    @Autowired
+    private LocationService locationService;
+
+    public Storyteller saveStoryteller(Storyteller storyteller) {
+        if (storytellerRepository.existsByEmail(storyteller.getEmail())) {
+            throw new RuntimeException("Email already exists: " + storyteller.getEmail());
+        }
+
+        // Ensure location exists
+        Location savedLocation = locationService.getLocationById(storyteller.getLocation().getLocation_Id());
+        storyteller.setLocation(savedLocation);
+
+        return storytellerRepository.save(storyteller);
+    }
+
+    public Storyteller getStorytellerById(Long id) {
+        return storytellerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Storyteller not found with id: " + id));
+    }
+
+    public List<Storyteller> getAllStorytellers() {
+        return storytellerRepository.findAll();
+    }
+
+    public List<Storyteller> getStorytellersByLocation(Long locationId) {
+        return storytellerRepository.findByLocation_Location_id(locationId);
+    }
 }
