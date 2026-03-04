04cf50520087f4ebdc5301ac5fb80fd6bb35dc2e
Omoyi <urakhel7@gmail.com>
2026-03-04 10:00:15 +0200

    Initial commit

---
+++ src/main/java/auca/ac/rw/literaturePreservation/OralLiteraturePreservationSystemApplication.java
@@ -0,0 +1,13 @@
+package auca.ac.rw.literaturePreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OralLiteraturePreservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OralLiteraturePreservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/auca/ac/rw/literaturePreservation/OralLiteraturePreservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package auca.ac.rw.literaturePreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OralLiteraturePreservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
