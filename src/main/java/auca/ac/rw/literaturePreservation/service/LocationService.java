62a1fc7381880797f8e70d3633660a0daa228a3b
Omoyi <urakhel7@gmail.com>
2026-03-04 11:27:33 +0200

    Get location by Id

--- src/main/java/auca/ac/rw/literaturePreservation/service/LocationService.java
+++ src/main/java/auca/ac/rw/literaturePreservation/service/LocationService.java
@@ -1,5 +1,19 @@
 package auca.ac.rw.literaturePreservation.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import auca.ac.rw.literaturePreservation.domain.Location;
+import auca.ac.rw.literaturePreservation.repository.LocationRepository;
+
+@Service
 public class LocationService {
 
+    @Autowired
+    private LocationRepository locationRepository;
+
+    public Location getLocationById(Long id) {
+        return locationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Location not found with id: " + id));
+    }
 }
