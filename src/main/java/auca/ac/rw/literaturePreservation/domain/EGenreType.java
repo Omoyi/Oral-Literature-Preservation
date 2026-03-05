c693952db8a498d7fad7f9ff7220c32ca2cb2563
Omoyi <urakhel7@gmail.com>
2026-03-05 21:30:08 +0200

    Categorize stories by genre

---
+++ src/main/java/auca/ac/rw/literaturePreservation/domain/EGenreType.java
@@ -0,0 +1,10 @@
+package auca.ac.rw.literaturePreservation.domain;
+
+public enum EGenreType {
+    IBISAKUZO,
+    IBYIVUGO,
+    IMIGANI_MIREMIRE,
+    IMIGANI_MIGUFI,
+    INDIRIMBO_Z_INKA,
+    IBISIGO
+}
