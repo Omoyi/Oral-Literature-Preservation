a7abbcc4c5ad4501947768e7e5ea08931cce816f
Omoyi <urakhel7@gmail.com>
2026-03-05 21:31:43 +0200

    Add comments for entity relations

--- src/main/java/auca/ac/rw/literaturePreservation/domain/Literature.java
+++ src/main/java/auca/ac/rw/literaturePreservation/domain/Literature.java
@@ -4,8 +4,13 @@ import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,6 +18,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,6 +35,9 @@ public class Literature {
 
     private String language; // Kinyarwanda, French, English.
 
+    @Enumerated(EnumType.STRING)
+    private EGenreType genre;
+
     private LocalDate recordedDate;
 
     // Many literature items can belong to one storyteller
@@ -36,15 +45,22 @@ public class Literature {
     @JoinColumn(name = "storyteller_id", nullable = false)
     private Storyteller storyteller;
 
-    // Many-to-Many with Tag
+    // Many literature items can belong to many different Tags and vice versa
     @ManyToMany
     @JoinTable(
             name = "literature_tags",
             joinColumns = @JoinColumn(name = "literature_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
-    
     private Set<Tag> tags = new HashSet<>();
+
+    //One literature has one audio recording, and one audio recording belongs to one literature.
+    @JsonIgnoreProperties({"literature"})
+    @OneToOne(mappedBy = "literature", cascade = CascadeType.ALL)
+    private AudioRecording audioRecording;
+
+    public AudioRecording getAudioRecording() { return audioRecording; }
+    public void setAudioRecording(AudioRecording audioRecording) { this.audioRecording = audioRecording; }
 
     public Literature() {}
 
@@ -75,4 +91,8 @@ public class Literature {
     public Set<Tag> getTags() { return tags; }
 
     public void setTags(Set<Tag> tags) { this.tags = tags; }
+
+    public EGenreType getGenre() { return genre; }
+
+    public void setGenre(EGenreType genre) { this.genre = genre; }
 }
